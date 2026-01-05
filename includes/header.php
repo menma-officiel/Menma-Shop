bf157711442dfccae098323fb2df98903c4a7300
menma-officiel <menma224.officiel@gmail.com>
2026-01-05 02:43:53 +0000

    correction de styles du index

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,15 @@
-<?php
-$root = "/boutique/"; 
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menma Shop</title>
-    <link rel="stylesheet" href="<?php echo $root; ?>assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <nav> <div class="logo">📱 Menma Shop</div>
+    <nav> <div class="logo">Menma Shop</div>
         <ul>
-            <li><a href="<?php echo $root; ?>index.php">Accueil</a></li>
+            <li><a href="index.php">Accueil</a></li>
             </ul>
     </nav>
     <main class="container">
