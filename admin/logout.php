760d0dd509a615c77930ee0119d753228858391c
menma-officiel <menma224.officiel@gmail.com>
2026-01-05 17:11:05 +0000

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,18 @@
 <?php
 session_start();
-session_destroy(); // Détruit la connexion
-header("Location: login.php");
+
+// 1. On vide la variable de session
+$_SESSION = array();
+
+// 2. On détruit physiquement la session
+if (session_id() != "" || isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 2592000, '/');
+}
+
+session_destroy();
+
+// 3. On force la redirection vers login.php
+// L'utilisation de ./ garantit qu'on reste dans le même dossier
+header("Location: ./login.php");
 exit();
 ?>
